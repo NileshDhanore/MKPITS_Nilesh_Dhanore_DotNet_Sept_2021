e3436becdd8078be6fae55763e6a945b3edebcce
Nilesh Dhanore <nilesh.sgd@gmail.com>
2021-10-18 11:41:52 +0530

    edit2

--- C_Programs/H_Oct_INT_MIN.c
+++ C_Programs/H_Oct_INT_MIN.c
@@ -1,14 +1,19 @@
-/* This is my Seventhth Assignment */
+/* To find the minimum value if integer data type */
 /* pre processor directive */
 #include<stdio.h>
 #include<limits.h>
 /* global variable declaration */
 int main()
 {
-int a=INT_MIN, b=SHRT_MIN, c=LONG_MIN, d=LLONG_MIN;
+char a=CHAR_MIN;
+signed char b=SCHAR_MIN;
+short c=SHRT_MIN;
+int d=INT_MIN;
+long e=LONG_MIN;
+long long f=LLONG_MIN;
 
 
-printf("\n Minimum Int value \na=%d \nb=%d \nc=%d \nd=%d",a,b,c,d);
+printf("\n Maximum Int value \na=%d \nb=%d \nc=%d \nd=%d \ne=%d \nf=%d",a,b,c,d,e,f);
 return 0;
 
 }
