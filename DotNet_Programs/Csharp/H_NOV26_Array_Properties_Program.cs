f4c67edc82d961db2949ffe0789b910115db85a4
Nilesh Dhanore <nilesh.sgd@gmail.com>
2021-11-28 03:25:38 +0530

    Csharp Assignments

---
+++ DotNet_Programs/Csharp/H_NOV26_Array_Properties_Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace H_NOV26_Array_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            Console.Write("cars name \n");
+            foreach (string f in cars)
+            {
+
+                Console.Write(" " + f + " ");
+            }
+            Console.WriteLine("\nThe size of cars array is " + cars.Length);
+            Array.Sort(cars);
+            Console.WriteLine(" \nSorted cars are");
+            foreach (string f in cars)
+            {
+
+                Console.Write(" " + f + " ");
+            }
+            Array.Reverse(cars);
+            Console.WriteLine("\n Sorted cars in decending oreder ");
+            foreach (string f in cars)
+            {
+
+                Console.Write(" " + f + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+    
+}
