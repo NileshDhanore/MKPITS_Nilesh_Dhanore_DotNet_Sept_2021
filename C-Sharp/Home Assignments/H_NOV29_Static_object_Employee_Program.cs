19827f885e63ea93677c50072043f2dc5a8082a4
Nilesh Dhanore <nilesh.sgd@gmail.com>
2021-12-18 03:19:32 +0530

    Allok

--- C-Sharp/Home Assignments/H_NOV29_Static_object_Employee_Program.cs
+++ C-Sharp/Home Assignments/H_NOV29_Static_object_Employee_Program.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 
-namespace H_NOV29_Static_object_Employee
+namespace H_Nov29_static_object_employee
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Employee e1 = new Employee(1, "Akash", 32000);
-            Console.WriteLine("The Gross salary : {0:C}",e1.GrossSalary());
-            Console.WriteLine("The Net salary : {0:C}", e1.NetSalary());
-            Employee.ShowCount();
-            Employee e2, e3, e4;
-            e2 = new Employee(2, "Hritvik", 30000);
-            e3 = new Employee(3, "Roshan", 40077);
-            e4 = new Employee(4, "Pooja", 67000);
-            Employee.ShowCount();
+            employee e1 = new employee(1, "Akash", 50000);
+            //gross salary
+            Console.WriteLine("The Gross Salary : {0:C}",e1.GrossSalary());   // to show the g.salary
+            //Net salary
+            Console.WriteLine("The Net Salary : {0:C}",e1.NetSalary());
+            employee.ShowCount();
+            employee e2, e3, e4;                   ///object has to be display in these way 
+            e2 = new employee(2,"Hritvik", 60000);
+            e3 = new employee(3,"Aman", 50000);
+            e4 = new employee(4,"Rishi", 40000);
+            employee.ShowCount();                 //to count the content 
+
         }
     }
 }
