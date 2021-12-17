feb1cb6b2832b6197bffe934a74acbedfa793777
Nilesh Dhanore <nilesh.sgd@gmail.com>
2021-12-18 03:16:39 +0530

    Data ok with Csharp

---
+++ SQL/Practice SQL/Employee_table.sql
@@ -0,0 +1,8 @@
+create table Employee(ID int primary key,firstname varchar(20),lastname varchar(20),branch varchar(20),city varchar(20));
+drop table Employee;
+insert into Employee values (101,'Akash','Narwade','Electrical','Nagpur');
+insert into Employee values (102,'Hritvik','Gayaki','Computer Science','Nagpur');
+insert into Employee values (103,'Nilesh','Dhanore','ETC','Nashik');
+insert into Employee values (104,'Roshan','Thakre','Mechanical','Nagpur');
+insert into Employee values (105,'Yashwant','Narwade','Mining','Pune');
+Select * from Employee; 
